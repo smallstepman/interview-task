e5ca3d31fd134f87658d32ab3c610454d36e98b2
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-02-19 21:00:32 +0100

    feat: read and parse csv input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #![allow(dead_code, unused_variables)]
 
+use csv::Reader;
+use serde::Deserialize;
 use std::collections::HashMap;
+use std::fs;
 
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
 enum TxType {
     Chargeback,
     Resolve,
@@ -10,6 +15,7 @@ enum TxType {
     Deposit,
 }
 
+#[derive(Deserialize, Debug)]
 enum TxState {
     Disputed,
     Resolved,
@@ -25,12 +31,31 @@ struct Client {
     total: u16, //?
 }
 
+#[derive(Deserialize, Debug)]
 struct Tx {
-    id: u32,
-    client_id: u16,
-    amount: f32, // optional
+    #[serde(alias = "type")]
     tx_type: TxType,
-    tx_state: Option<TxState>,
+    #[serde(alias = "client")]
+    client_id: u16,
+    #[serde(alias = "tx")]
+    id: u32,
+    amount: f32,               // optional
+    tx_state: Option<TxState>, // optional
 }
 
-fn main() {}
+fn main() -> Result<(), csv::Error> {
+    let clients: Vec<Client> = vec![];
+    let transactions: HashMap<u32, Tx> = HashMap::new();
+    let csv = fs::read_to_string("tests/test_cases/in1.csv")?.replace(" ", "");
+    let mut reader = Reader::from_reader(csv.as_bytes());
+    for record in reader.deserialize() {
+        let tx: Tx = record?;
+        println!("{:?}", tx);
+    }
+
+    Ok(())
+}
+
+fn get_transaction_stream() -> Tx {
+    todo!()
+}
