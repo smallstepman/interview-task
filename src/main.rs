6a528b7f391a28dd65dbf85658a92cdc9d6fbae0
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-02-19 20:34:37 +0100

    feat: create typesystem skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code, unused_variables)]
+
+use std::collections::HashMap;
+
+enum TxType {
+    Chargeback,
+    Resolve,
+    Dispute,
+    Withdrawal,
+    Deposit,
 }
+
+enum TxState {
+    Disputed,
+    Resolved,
+    Chargebacked,
+}
+
+struct Client {
+    id: u16,
+    transaction_history: HashMap<u32, Tx>,
+    locked: bool,
+    available: u16,
+    held: u16,
+    total: u16, //?
+}
+
+struct Tx {
+    id: u32,
+    client_id: u16,
+    amount: f32, // optional
+    tx_type: TxType,
+    tx_state: Option<TxState>,
+}
+
+fn main() {}
