a29665e23138ca2a2c7ffa3e0ff6359c23a3a635
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-02-20 18:01:15 +0100

    fix(error_macro): change visibility to public

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 macro_rules! build_custom_error {
     ($err_name:ident, $err_msg:literal) => {
         #[derive(Debug)]
-        struct $err_name;
+        pub(crate) struct $err_name;
         impl Error for $err_name {}
         impl fmt::Display for $err_name {
             fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
