05e0583e8795664156ef1ac7ee7ba926c6458144
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-02-22 03:33:01 +0100

    test: add unit tests to accounts.rs

--- src/accounts.rs
+++ src/accounts.rs
@@ -8,7 +8,7 @@ use serde::{
 use std::{collections::HashMap, error::Error, fmt};
 
 pub(crate) type ClientId = u16;
-type ClientActionResult = Result<(), Box<dyn Error>>;
+type ClientResult = Result<(), Box<dyn Error>>;
 
 #[derive(Default, Debug)]
 pub(crate) struct Accounts(HashMap<ClientId, Client>);
@@ -23,9 +23,9 @@ impl Accounts {
         // Hashmap's hashing algorithm is randomly seeded.
         // Here, we're collecting HashMap values into Vec, to then sort the clients
         // based on their ID, because this ensures deterministic output of this function
-        // which in turn makes testing much easier as there is no need to account for
-        // all different output combinations of the same data. This affects performance,
-        // but given the purposes or this task, I'm happy to accept it.
+        // which in turn makes integration testing much easier as there is no need to
+        // account for all different output combinations of the same data.
+        // This affects performance, but given the purposes or this task, I'm happy to accept it.
         let mut clients = self.0.values().collect::<Vec<_>>();
         clients.sort_by_key(|c| c.id);
         for client in clients {
@@ -56,31 +56,31 @@ impl Client {
     pub(crate) fn freeze(&mut self) {
         self.locked = true;
     }
-    pub(crate) fn deposit(&mut self, amount: Decimal) -> ClientActionResult {
+    pub(crate) fn deposit(&mut self, amount: Decimal) -> ClientResult {
         self.is_unlocked()?;
         self.available += amount;
         Ok(())
     }
-    pub(crate) fn withdrawal(&mut self, amount: Decimal) -> ClientActionResult {
+    pub(crate) fn withdrawal(&mut self, amount: Decimal) -> ClientResult {
         self.is_unlocked()?;
         self.has_enough_funds(amount)?;
         self.available -= amount;
         Ok(())
     }
-    pub(crate) fn hold(&mut self, amount: Decimal) -> ClientActionResult {
+    pub(crate) fn hold(&mut self, amount: Decimal) -> ClientResult {
         self.is_unlocked()?;
         self.has_enough_funds(amount)?;
         self.available -= amount;
         self.held += amount;
         Ok(())
     }
-    pub(crate) fn resolve(&mut self, amount: Decimal) -> ClientActionResult {
+    pub(crate) fn resolve(&mut self, amount: Decimal) -> ClientResult {
         self.is_unlocked()?;
         self.held -= amount;
         self.available += amount;
         Ok(())
     }
-    pub(crate) fn chargeback(&mut self, amount: Decimal) -> ClientActionResult {
+    pub(crate) fn chargeback(&mut self, amount: Decimal) -> ClientResult {
         self.held -= amount;
         Ok(())
     }
@@ -103,20 +103,21 @@ impl Client {
     }
 }
 
+fn round(d: Decimal) -> Decimal {
+    let mut normalized = d.normalize();
+    if normalized.scale() == 0 {
+        normalized.rescale(1);
+    } else if normalized.scale() > 4 {
+        normalized = normalized.round_dp(4);
+    }
+    normalized
+}
+
 impl Serialize for Client {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let round = |d: Decimal| {
-            let mut normalized = d.normalize();
-            if normalized.scale() == 0 {
-                normalized.rescale(1);
-            } else if normalized.scale() > 4 {
-                normalized = normalized.round_dp(4);
-            }
-            normalized
-        };
         let mut s = serializer.serialize_struct("Client", 5)?;
         s.serialize_field("client", &self.id)?;
         s.serialize_field("available", &round(self.available))?;
@@ -129,3 +130,57 @@ impl Serialize for Client {
 
 build_custom_error!(NotEnoughFunds, "Not enough funds in client's account.");
 build_custom_error!(AccountLocked, "Unable to modify locked account.");
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decline_overdraft_attempt() {
+        let mut c = Client::new(123);
+        c.available = Decimal::ONE;
+        let op = c.withdrawal(Decimal::new(100, 0));
+        assert!(op.is_err());
+    }
+
+    #[test]
+    fn decline_withdrawal_from_frozen_account() {
+        let mut c = Client::new(123);
+        c.available = Decimal::ONE;
+        c.locked = true;
+        let op = c.withdrawal(Decimal::new(1, 1));
+        assert!(op.is_err());
+    }
+
+    #[test]
+    fn correct_decimal_formatting() {
+        let mut c = Client::new(123);
+        c.available = Decimal::new(12322344, 5);
+        assert_eq!(round(c.available).to_string(), "123.2234");
+        c.available = Decimal::new(12322344, 0);
+        assert_eq!(round(c.available).to_string(), "12322344.0");
+        c.available = Decimal::new(1440000000, 7);
+        assert_eq!(round(c.available).to_string(), "144.0");
+        c.available = Decimal::new(1440200000, 7);
+        assert_eq!(round(c.available).to_string(), "144.02");
+        c.available = Decimal::new(1440020000, 7);
+        assert_eq!(round(c.available).to_string(), "144.002");
+        c.available = Decimal::new(1440002001, 7);
+        assert_eq!(round(c.available).to_string(), "144.0002");
+    }
+
+    #[test]
+    fn correct_floating_point_operations() {
+        let mut c = Client::new(123);
+        c.deposit(Decimal::new(123223412341243344, 14)).ok();
+        c.deposit(Decimal::new(123223777700000007, 16)).ok();
+        assert_eq!(round(c.available).to_string(), "1244.5565");
+    }
+
+    #[test]
+    fn freeze_account() {
+        let mut c = Client::new(123);
+        c.freeze();
+        assert!(c.locked);
+    }
+}
